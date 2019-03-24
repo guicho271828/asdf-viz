4a7f3b3495ab6cf2372ff8b944088e27934587cb
Masataro Asai <guicho2.71828@gmail.com>
2019-03-25 00:54:45 +0900

    testing

--- test.sh
+++ test.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 ros install asdf-viz
+
+asdf-viz -l asdf-viz.png asdf-viz
+asdf-viz -l weblocks.png weblocks
+asdf-viz -l spinneret.png spinneret
+
+
+
 call-graph-viz -s alexandria -p alexandria -f alexandria:symbolicate symbolicate.png
 call-graph-viz -i -s alexandria -f alexandria:symbolicate symbolicate2.png
 call-graph-viz -s alexandria -p alexandria alexandria.png
+
+
+class-viz asdf.png asdf:component
+class-viz -s plump plump.png plump:node
